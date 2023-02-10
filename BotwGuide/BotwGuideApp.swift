6acfb1b79d959c474991a0cbf02c53ff85dfacc5
Hawken Rives <hawkrives@fastmail.fm>
2023-02-09 19:04:06 -0500

    Initial Commit

---
+++ BotwGuide/BotwGuideApp.swift
@@ -0,0 +1,17 @@
+//
+//  BotwGuideApp.swift
+//  BotwGuide
+//
+//  Created by Hawken Rives on 2/8/23.
+//
+
+import SwiftUI
+
+@main
+struct BotwGuideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
